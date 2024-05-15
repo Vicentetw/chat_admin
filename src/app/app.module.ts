4fe993fedbf6e733a87f3b682dc5a748aa35c61b
Vicentetw <perrottavicente@gmail.com>
2024-05-15 11:14:15 -0300

    init Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
+import { QuestionFormComponent } from './components/question-form/question-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionListComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
